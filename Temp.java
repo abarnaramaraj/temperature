0dbb1b271ed4030d0ad541ed867bc01c9871d71f
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-07 11:57:14 +0530

    Create Temp.java

---
+++ Temp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+public class Temp
+{
+public static void main(String[] args)
+{
+int o=273,i;
+int c;
+Scanner sc=new Scanner(System.in);
+c=sc.nextInt();
+i=(o+c);
+System.out.println(i);
+}
+}
